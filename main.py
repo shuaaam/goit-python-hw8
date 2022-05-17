dc09050798ac2da03ac037934c765527b5bcae71
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-05-17 21:49:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,32 @@ users = [
     },
     {
         'name': 'Jill',
-        'birthday': '15 May 2000'
+        'birthday': '17 May 2000'
     },
     {
         'name': 'Kim',
-        'birthday': '20 May 2000'
+        'birthday': '19 May 2000'
     },
     {
         'name': 'Jan',
         'birthday': '20 May 2000'
     }
 ]
+
 today = datetime.today()
 interval = today + timedelta(days=7)
-range_d = range(today, interval)
 
 
 def get_birthdays_per_week(users):
     for i in users:
         birthday = datetime.strptime(i["birthday"], "%d %B %Y")
         theday = datetime(today.year, birthday.month, birthday.day)
-        if theday.today in range_d:
+        if today <= theday.today() <= interval:
             day = today.weekday()
             if day == 0:
                 print("Monday: " + i['name'])
             if day == 1:
-                print("Tuesday: ", + i['name'])
+                print("Tuesday: " + i['name'])
             if day == 2:
                 print("Wednesday: " + i['name'])
             if day == 3:
